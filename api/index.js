3b4242a23b55d040e3454fe5a2a25a34ebc7b7fa
tramaliya <94732571+tramaliya@users.noreply.github.com>
2021-11-20 14:08:15 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 // Bot Setting
 const TelegramBot = require('node-telegram-bot-api');
-const token = '2133290397:AAHIgB1ZusJ-m2-TluhcnWMfZQNwfRPMk1U';
+const token = '2135832460:AAHvVbCAx_EqWe7Ep-_XHcf4uN-ifR-KIG4';
 const bot = new TelegramBot(token, {polling: true});
 
 
