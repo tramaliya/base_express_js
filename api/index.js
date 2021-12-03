fb4f7fc52a760cc47f90ad43b92d9848337366aa
tramaliya <94732571+tramaliya@users.noreply.github.com>
2021-12-03 22:43:56 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -24,7 +24,7 @@ bot.onText(/\/show_url/, (msg) => {
     bot.sendMessage(
         global_msg_id,
         `
-            Contoh URL::/nhttps://esp-telebot.herokuapp.com/api/sensor/31/64/97 \n
+            Contoh URL:: https://esp-telebot.herokuapp.com/api/sensor/31/64/97 \n
             https://esp-telebot.herokuapp.com/api/test/testmenu
         `
     );
